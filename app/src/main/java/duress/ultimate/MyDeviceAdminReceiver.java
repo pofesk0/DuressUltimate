b01cfd3474982c8f6217e3424b3e59c583de3658
pofesk0 <pofesk0@proton.me>
2026-08-29 07:28:43 +0500

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/duress/ultimate/MyDeviceAdminReceiver.java
+++ app/src/main/java/duress/ultimate/MyDeviceAdminReceiver.java
@@ -1,19 +1,30 @@
 package duress.ultimate;
 
-import android.app.*;
-import android.app.admin.*;
-import android.content.*;
-import android.widget.*;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
 public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
-    	
+        
     @Override
-    public void onEnabled(Context context, Intent intent) {
-        Toast.makeText(context,"Device Admin Enabled", Toast.LENGTH_SHORT).show();
+    public void onEnabled(Context context, Intent intent) { 
+        setWipeLimit(context);
+        Toast.makeText(context, "Device Admin Enabled", Toast.LENGTH_SHORT).show();        
+    }
+
+    private void setWipeLimit(Context context) {
+        try {
+            DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+            ComponentName adminName = new ComponentName(context, MyDeviceAdminReceiver.class);
+            dpm.setMaximumFailedPasswordsForWipe(adminName, 1);
+        } catch (Throwable ignored) {} 
     }
 
     @Override
     public void onDisabled(Context context, Intent intent) {
-        Toast.makeText(context,"Device Admin Disabled", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "Device Admin Disabled", Toast.LENGTH_SHORT).show();
     }
 }
